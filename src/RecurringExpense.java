46383eff400dc653e45193146760780a05161112
PiotrKosakowski <piotr.kosakowski2.stud@pw.edu.pl>
2022-12-28 18:48:45 +0100

    rozbudowany RecurringExpense

--- src/RecurringExpense.java
+++ src/RecurringExpense.java
@@ -1,6 +1,36 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class RecurringExpense extends Expense {
-    public RecurringExpense(double amount) {
+    protected FrequencyOfExpsense frequency;
+//    protected int numberOfTimes;
+    protected ArrayList<LocalDate> datesOfPayments;
+
+    public RecurringExpense(double amount, FrequencyOfExpsense frequency, int numberOfTimes) {
         super(amount);
         this.id = "R" + this.id;
+        this.datesOfPayments = new ArrayList<>();
+        this.datesOfPayments.add(0, this.date);
+            switch (frequency.id) {
+                case 1:
+                    for(int i=1; i<numberOfTimes;i++) {
+                        this.datesOfPayments.add(0, this.datesOfPayments.get(i-1).plusDays(1));
+                    }
+                case 2:
+                    for(int i=1; i<numberOfTimes;i++) {
+                        this.datesOfPayments.add(0, this.datesOfPayments.get(i-1).plusWeeks(1));
+                    }
+                case 3:
+                    for(int i=1; i<numberOfTimes;i++) {
+                        this.datesOfPayments.add(0, this.datesOfPayments.get(i-1).plusMonths(1));
+                    }
+                case 4:
+                    for(int i=1; i<numberOfTimes;i++) {
+                        this.datesOfPayments.add(0, this.datesOfPayments.get(i-1).plusYears(1));
+                    }
+            }
     }
+
+
 }
+
