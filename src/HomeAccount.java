1c4d92413ad683d5e1e015ce9ffa734b22038e50
MagdalenaJeczen <01171155@pw.edu.pl>
2022-12-28 18:18:48 +0100

    madzia2

--- src/HomeAccount.java
+++ src/HomeAccount.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeAccount {
+    private static List<Expense> expenses;
+    private static List<Income> incomings;
+    private static List<Investment> investments;
+    private List<User> users;
+    private double balance;
+
+    public HomeAccount(){
+        this.expenses = new ArrayList<>();
+        this.incomings = new ArrayList<>();
+        this.investments = new ArrayList<>();
+        this.users = new ArrayList<>();
+    }
+
+    private static void addIncoming(Income income){
+        incomings.add(income);
+    }
+
+    private static void addExpense(Expense expense){
+        expenses.add(expense);
+    }
+
+    private static void addInvestment(Investment investment){
+        investments.add(investment);
+    }
+
+    private static void removeIncoming()
 }
