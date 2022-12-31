0741abd3b3f6b5ba45eaaab39c0145ac5b2e26e8
karim <01171252@pw.edu.pl>
2022-12-31 01:20:06 +0100

    31.12.2022 poprawione

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 import java.time.LocalDate;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Dziala");
+        HomeAccount ha= new HomeAccount();
+        UserAdult u = new UserAdult(ha,"sda","sfa");
+        LocalDate date = LocalDate.of(2023, 1, 8);
+        Expense e = new Expense(456,CategoryOfExpense.GROCERIES,false);
+
+        u.addExpense(e);
+        double a = u.getPersonalBalance(date);
+        System.out.println(a);
     }
-    HomeAccount ha= new HomeAccount();
-    UserAdult u1 = new UserAdult(ha,"sda","sfa");
-    LocalDate date = LocalDate.of(2020, 1, 8);
-    Expense e = new Expense(456,CategoryOfExpense.GROCERIES,false);
-    u1.addExpense(e);
-    u1.getExpenses(LocalDate.now);
+
 }
